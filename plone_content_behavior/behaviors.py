b5464fb058f15ebe8705dd7abbf8e8e37f82f643
Alex Clark <aclark@aclark.net>
2015-06-20 09:12:14 -0400

    MF

--- plone_content_behavior/behaviors.py
+++ plone_content_behavior/behaviors.py
@@ -1,8 +1,15 @@
+from plone.autoform.interfaces import IFormFieldProvider
 from plone.supermodel import model
 from zope import schema
+from zope.interface import alsoProvides
 
 
 class IDoStuff(model.Schema):
     """
     """
     stuff = schema.TextLine(title=u"Do Stuff")
+
+
+alsoProvides(IDoStuff, IFormFieldProvider)
+
+
