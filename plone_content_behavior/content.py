645ba1199ee3bdd88b79a627ba6fd7830f81c5eb
Alex Clark <aclark@aclark.net>
2015-06-20 08:15:43 -0400

    Add files

--- plone_content_behavior/content.py
+++ plone_content_behavior/content.py
@@ -2,8 +2,8 @@ from plone.supermodel import model
 from zope import schema
 
 
-class Thing(model.Schema)
+class Thing(model.Schema):
     """A Thing"""
 
 
-    title = schema.TextLine(title="Name of Thing")
+    title = schema.TextLine(title=u"Name of Thing")
